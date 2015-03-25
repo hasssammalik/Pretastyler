092e5e5b08683d7a96b63a8cc8b68b100f44ef6b
anil3a <anilprz3@gmail.com>
2015-03-25 16:13:34 +1100

    changed base url to m1

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://pretastyler.com/';
+$config['base_url']	= 'http://m1.pretastyler.com/';
 
 /*
 |--------------------------------------------------------------------------
