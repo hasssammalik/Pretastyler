01c1c781e2660400c031dc834ec2ea00ef5b5fef
diwaskandel <diwas@asmex.com.au>
2015-04-02 11:15:35 +1100

    faq page

--- application/views/catalog/faq.php
+++ application/views/catalog/faq.php
@@ -41,9 +41,9 @@
 					</div>
 					<div class="faq-body">
 						A fashion retail platform which revolutionizes the way women shop online by creating individually stocked malls filled with items specifically selected to flatter their shape, age and style preferences.   
-<br>Like your fingerprints, you own a unique set of body characteristics and personal style preferences that impact which types of garments and accessories suit you.
-<br>With PrêtàStyler’s advanced technology you can have a fashion mall created exclusively for you, filled with clothes and accessories that are perfectly matched to flatter your shape.
-<br>To complete your look and ensure you look and feel amazing every time you walk out the door each garment also comes with expert style advice on how to wear the garment to perfection.
+						<br>Like your fingerprints, you own a unique set of body characteristics and personal style preferences that impact which types of garments and accessories suit you.
+						<br>With PrêtàStyler’s advanced technology you can have a fashion mall created exclusively for you, filled with clothes and accessories that are perfectly matched to flatter your shape.
+						<br>To complete your look and ensure you look and feel amazing every time you walk out the door each garment also comes with expert style advice on how to wear the garment to perfection.
 					</div>
 				</div>
 				<div class="faq-info" id="q2">
@@ -60,23 +60,56 @@
 				</div>
 			</div>
 			<div class="faq-info" id="q2a">
-					<div class="faq-header">
-						B. WHAT'S IN THE SNEAKY FINE PRINT, WHERE'S THE CATCH?
-					</div>
-					<div class="faq-body">
-						It’s so cool to see someone concerned enough to ask! We care about protecting our member’s interests’ too. That being said we have nothing to hide, and we welcome you to view our <a href="/terms.html">Terms of Use</a>.
-					</div>
+				<div class="faq-header">
+					B. WHAT'S IN THE SNEAKY FINE PRINT, WHERE'S THE CATCH?
 				</div>
-				<div class="faq-info" id="q2b">
-					<div class="faq-header">
-						C. FREE TRIAL PERIOD
-					</div>
-					<div class="faq-body">
-						Membership comes with a 14 day free trial the period.  A counter on the menu bar of each page will show the days you have left.<br>
-Over your first week you will be sent introductory emails that explain how to get the most benefit from PrêtàStyler.
+				<div class="faq-body">
+					It’s so cool to see someone concerned enough to ask! We care about protecting our member’s interests’ too. That being said we have nothing to hide, and we welcome you to view our <a href="/terms.html">Terms of Use</a>.
 				</div>
 			</div>
-</div>
+			<div class="faq-info" id="q2b">
+				<div class="faq-header">
+					C. FREE TRIAL PERIOD
+				</div>
+				<div class="faq-body">
+					Membership comes with a 14 day free trial the period.  A counter on the menu bar of each page will show the days you have left.<br>
+					Over your first week you will be sent introductory emails that explain how to get the most benefit from PrêtàStyler.
+				</div>
+			</div>
+
+			<div class="faq-info" id="q2c">
+				<div class="faq-header">
+					E. UPDATING YOUR DETAILS
+				</div>
+				<div class="faq-body">
+					Whenever necessary, you can update your details by clicking your [Account] area located
+					at the top right hand side of the page
+				</div>
+			</div>
+
+			<div class="faq-info" id="q2d">
+				<div class="faq-header">
+					F. HOW TO LOGIN?
+				</div>
+				<div class="faq-body">
+					<ul>
+						<li>Click [LOGIN] located in the top right corner of every page.</li>
+						<li>A login panel will open up.</li>
+						<li>Enter the email address you signed up with and your password</li>
+						<li>Finally, click the LOGIN button.</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="faq-info" id="q2e">
+				<div class="faq-header">
+					G. DELETING YOUR ACCOUNT
+				</div>
+				<div class="faq-body">
+					We’d be really sad to see you go! But if you must, please send an email to <a href="mailto:info@pretastyler.com">info@pretastyler.com</a> requesting your account be deleted and we will personally make sure your account is removed promptly.  Be sure to also give us the email address you use on the site and tell us whether or not you still want to receive our company updates. In addition you will need to stop your monthly payments through Paypal
+				</div>
+			</div>
+		</div>
 	</div>
 	<div class="half">
 		<div class="faqs">
@@ -89,12 +122,13 @@ Over your first week you will be sent introductory emails that explain how to ge
 				</div>
 				<div style="padding:10px 50px;">
 					<ul>
-						<li><a href="#q2a" class="i">a. How Do I Sign Up and What's Required?</a></li>
-						<li><a href="#q2b" class="i">b. What's in the sneaky fine print, where's the catch?</a></li>
-						<li><a href="#q2c" class="i">c. Is There a Free Version?</a></li>
-						<li><a href="#q2d" class="i">d. Free Trials</a></li>
-						<li><a href="#q2e" class="i">e. Updating Your Details</a></li>
-						<li><a href="#q2f" class="i">f. Deleting Your Account</a></li>
+						<li><a href="#q2" class="i">a. How Do I Sign Up and What's Required?</a></li>
+						<li><a href="#q2a" class="i">b. What's in the sneaky fine print, where's the catch?</a></li>
+						<li><a href="#q2b" class="i">c. Is There a Free Version?</a></li>
+						<li><a href="#q2b" class="i">d. Free Trials</a></li>
+						<li><a href="#q2d" class="i">e. Updating Your Details</a></li>
+						<li><a href="#q2e" class="i">f. How to Login</a></li>
+						<li><a href="#q2f" class="i">g. Deleting Your Account</a></li>
 					</ul>
 				</div>
 				<div><a href="#q3"><strong>YOUR PASSWORD</strong></a></div>
