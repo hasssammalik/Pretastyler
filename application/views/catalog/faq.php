9f76fed9c115bb6bfa4822a9a062e778684b66ab
diwaskandel <diwas@asmex.com.au>
2015-04-02 11:06:43 +1100

    faq page

--- application/views/catalog/faq.php
+++ application/views/catalog/faq.php
@@ -37,13 +37,13 @@
 			<div class="qanda">
 				<div class="faq-info" id="q1">
 					<div class="faq-header">
-						WHAT IS PRÊT À STYLER
+						WHAT IS PRÊTÀSTYLER?
 					</div>
 					<div class="faq-body">
-						A fashion retail platform which revolutionizes the way women shop online by creating individually stocked malls filled with items specifically selected to flatter your their shape, age and style preferences.   
-						Like your fingerprints, you own a unique set of body characteristics and personal style preferences that impact which types of garments and accessories suit you.
-						<br><br>With PrêtàStyler’s advanced technology you can have a fashion mall created exclusively for you, filled with clothes and accessories that are perfectly matched to flatter your shape.
-						To complete your look and ensure you look and feel amazing every time you walk out the door each garment also comes with expert style advice on how to wear the garment to perfection.
+						A fashion retail platform which revolutionizes the way women shop online by creating individually stocked malls filled with items specifically selected to flatter their shape, age and style preferences.   
+<br>Like your fingerprints, you own a unique set of body characteristics and personal style preferences that impact which types of garments and accessories suit you.
+<br>With PrêtàStyler’s advanced technology you can have a fashion mall created exclusively for you, filled with clothes and accessories that are perfectly matched to flatter your shape.
+<br>To complete your look and ensure you look and feel amazing every time you walk out the door each garment also comes with expert style advice on how to wear the garment to perfection.
 					</div>
 				</div>
 				<div class="faq-info" id="q2">
@@ -59,28 +59,21 @@
 					</p><p>4. Confirm the same Password</p>
 				</div>
 			</div>
-			<div class="faq-info" id="q3">
+			<div class="faq-info" id="q2a">
 					<div class="faq-header">
-						WHAT IS PRÊT À STYLER
+						B. WHAT'S IN THE SNEAKY FINE PRINT, WHERE'S THE CATCH?
 					</div>
 					<div class="faq-body">
-						A fashion retail platform which revolutionizes the way women shop online by creating individually stocked malls filled with items specifically selected to flatter your their shape, age and style preferences.   
-						Like your fingerprints, you own a unique set of body characteristics and personal style preferences that impact which types of garments and accessories suit you.
-						<br><br>With PrêtàStyler’s advanced technology you can have a fashion mall created exclusively for you, filled with clothes and accessories that are perfectly matched to flatter your shape.
-						To complete your look and ensure you look and feel amazing every time you walk out the door each garment also comes with expert style advice on how to wear the garment to perfection.
+						It’s so cool to see someone concerned enough to ask! We care about protecting our member’s interests’ too. That being said we have nothing to hide, and we welcome you to view our <a href="/terms.html">Terms of Use</a>.
 					</div>
 				</div>
-				<div class="faq-info" id="q4">
+				<div class="faq-info" id="q2b">
 					<div class="faq-header">
-						A. HOW DO I SIGN UP AND WHAT'S REQUIRED?
+						C. FREE TRIAL PERIOD
 					</div>
 					<div class="faq-body">
-						<p>Registration is through this [page] and costs $9 per month.</p>
-						<p>	Once you have registered you will be lead through the profile and preferences set-up. Upon completion your personalized mall will be stocked and the fun begins!
-							<p>1. Click <a href="/index.html#profile">HERE</a> or the SIGN-UP link on the top right of every page. (You can also sign up using any of the signup buttons throughout the website)
-							</p><p>	2. Enter you Name and Email
-						</p><p>	3. Chosse a Password (must be alphanumberic - letters and numbers only)
-					</p><p>4. Confirm the same Password</p>
+						Membership comes with a 14 day free trial the period.  A counter on the menu bar of each page will show the days you have left.<br>
+Over your first week you will be sent introductory emails that explain how to get the most benefit from PrêtàStyler.
 				</div>
 			</div>
 </div>
@@ -96,12 +89,12 @@
 				</div>
 				<div style="padding:10px 50px;">
 					<ul>
-						<li>a. How Do I Sign Up and What's Required?</li>
-						<li>What's in the sneaky fine print, where's the catch?</li>
-						<li>Is There a Free Version?</li>
-						<li>Free Trials</li>
-						<li>Updating Your Details</li>
-						<li>Deleting Your Account</li>
+						<li><a href="#q2a" class="i">a. How Do I Sign Up and What's Required?</a></li>
+						<li><a href="#q2b" class="i">b. What's in the sneaky fine print, where's the catch?</a></li>
+						<li><a href="#q2c" class="i">c. Is There a Free Version?</a></li>
+						<li><a href="#q2d" class="i">d. Free Trials</a></li>
+						<li><a href="#q2e" class="i">e. Updating Your Details</a></li>
+						<li><a href="#q2f" class="i">f. Deleting Your Account</a></li>
 					</ul>
 				</div>
 				<div><a href="#q3"><strong>YOUR PASSWORD</strong></a></div>
