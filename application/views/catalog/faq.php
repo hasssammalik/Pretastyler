a096ecc6193a9ec0eeef333e62bb29b2a889bb21
diwaskandel <diwas@asmex.com.au>
2015-04-02 11:48:46 +1100

    faq page

--- application/views/catalog/faq.php
+++ application/views/catalog/faq.php
@@ -1,4 +1,4 @@
-<article class="faq">
+<div class="faq">
 	<div class="container-full">
 		<div class="half">
 			<div class="qanda">
@@ -21,145 +21,111 @@
 						<p>Registration is through this [page] and costs $9 per month.</p>
 						<p>	Once you have registered you will be lead through the profile and preferences set-up. Upon completion your personalized mall will be stocked and the fun begins!
 							<p>1. Click <a href="/index.html#profile">HERE</a> or the SIGN-UP link on the top right of every page. (You can also sign up using any of the signup buttons throughout the website)
-							</p><p>	2. Enter you Name and Email
-						</p><p>	3. Chosse a Password (must be alphanumberic - letters and numbers only)
-					</p><p>4. Confirm the same Password</p>
+								</p><p>	2. Enter you Name and Email
+								</p><p>	3. Chosse a Password (must be alphanumberic - letters and numbers only)
+							</p><p>4. Confirm the same Password</p>
+						</div>
+					</div>
+					<div class="faq-info" id="q2a">
+						<div class="faq-header">
+							B. WHAT'S IN THE SNEAKY FINE PRINT, WHERE'S THE CATCH?
+						</div>
+						<div class="faq-body">
+							It’s so cool to see someone concerned enough to ask! We care about protecting our member’s interests’ too. That being said we have nothing to hide, and we welcome you to view our <a href="/terms.html">Terms of Use</a>.
+						</div>
+					</div>
+					<div class="faq-info" id="q2b">
+						<div class="faq-header">
+							C. FREE TRIAL PERIOD
+						</div>
+						<div class="faq-body">
+							Membership comes with a 14 day free trial the period.  A counter on the menu bar of each page will show the days you have left.<br>
+							Over your first week you will be sent introductory emails that explain how to get the most benefit from PrêtàStyler.
+						</div>
+					</div>
+					
+					<div class="faq-info" id="q2c">
+						<div class="faq-header">
+							E. UPDATING YOUR DETAILS
+						</div>
+						<div class="faq-body">
+							Whenever necessary, you can update your details by clicking your [Account] area located
+							at the top right hand side of the page
+						</div>
+					</div>
+					
+					<div class="faq-info" id="q2d">
+						<div class="faq-header">
+							F. HOW TO LOGIN?
+						</div>
+						<div class="faq-body">
+							<ul style="list-style:none;">
+								<li>Click [LOGIN] located in the top right corner of every page.</li>
+								<li>A login panel will open up.</li>
+								<li>Enter the email address you signed up with and your password</li>
+								<li>Finally, click the LOGIN button.</li>
+							</ul>
+						</div>
+					</div>
+					<div class="faq-info" id="q2e">
+						<div class="faq-header">
+							G. DELETING YOUR ACCOUNT
+						</div>
+						<div class="faq-body">
+							We’d be really sad to see you go! But if you must, please send an email to <a href="mailto:info@pretastyler.com">info@pretastyler.com</a> requesting your account be deleted and we will personally make sure your account is removed promptly.  Be sure to also give us the email address you use on the site and tell us whether or not you still want to receive our company updates. In addition you will need to stop your monthly payments through Paypal
+						</div>
+					</div>
+					
+					<div class="faq-info" id="q3">
+						<div class="faq-header">
+							YOUR PASSWORD
+						</div>
+						<div class="faq-body">
+							&nbsp;&nbsp;&nbsp;a. Forgetten Your Password?<br>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No problem, just follow these easy steps:
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<p>
+								<ol>
+									<li>Click the ‘Forgot Your Password’ link below the LOG IN button or <a href="/useraccount/login.html">HERE</a>.</li>
+									<li>On the next screen, you will be asked to enter your email address.</li>
+									<li>Click Send.</li>
+									<li>You will receive and email to confirm the request is from you with a reset link.  If you don’t at first see it remember to check your junk/spam folder.</li>
+								</ol>
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="faq-info" id="q2a">
-				<div class="faq-header">
-					B. WHAT'S IN THE SNEAKY FINE PRINT, WHERE'S THE CATCH?
+		</div>
+		<div class="half">
+			<div class="faqs">
+				<div class="faqs-header">FAQs:</div>
+				<div class="tab-links">
+					<div class="active"><a href="#q1"><strong>WHAT IS PRÊTÀSTYLER?</strong></a></div>
+					
+					<div style="font-style:italic;">
+						<a href="#q2"><strong>SIGNING UP/ MEMBERSHIP</strong></a>
+					</div>
+					<div style="padding:10px 50px;">
+						<ul>
+							<li><a href="#q2" class="i">a. How Do I Sign Up and What's Required?</a></li>
+							<li><a href="#q2a" class="i">b. What's in the sneaky fine print, where's the catch?</a></li>
+							<li><a href="#q2b" class="i">c. Is There a Free Version?</a></li>
+							<li><a href="#q2b" class="i">d. Free Trials</a></li>
+							<li><a href="#q2c" class="i">e. Updating Your Details</a></li>
+							<li><a href="#q2d" class="i">f. How to Login</a></li>
+							<li><a href="#q2e" class="i">g. Deleting Your Account</a></li>
+						</ul>
+					</div>
+					<div><a href="#q3"><strong>YOUR PASSWORD</strong></a></div>
+					<div><a href="#q4"><strong>YOUR PROFILE</strong></a></div>
+					<div><a href="#q5"><strong>THE MALL</strong></a></div>
+					<div><a href="#q6"><strong>OUR SEARCHES</strong></a></div>
+					<div><a href="#q7"><strong>STYLE GENIE APP</strong></a></div>
+					<div><a href="#q8"><strong>STYLE CLINIC</strong></a></div>
+					<div><a href="#q9"><strong>BUGS, SUGGESTIONS & FEEDBACK</strong></a></div>
+					<div><a href="#q10"><strong>MY PRIVATE STYLIST</strong></a></div>
 				</div>
-				<div class="faq-body">
-					It’s so cool to see someone concerned enough to ask! We care about protecting our member’s interests’ too. That being said we have nothing to hide, and we welcome you to view our <a href="/terms.html">Terms of Use</a>.
-				</div>
-			</div>
-			<div class="faq-info" id="q2b">
-				<div class="faq-header">
-					C. FREE TRIAL PERIOD
-				</div>
-				<div class="faq-body">
-					Membership comes with a 14 day free trial the period.  A counter on the menu bar of each page will show the days you have left.<br>
-					Over your first week you will be sent introductory emails that explain how to get the most benefit from PrêtàStyler.
-				</div>
-			</div>
-
-			<div class="faq-info" id="q2c">
-				<div class="faq-header">
-					E. UPDATING YOUR DETAILS
-				</div>
-				<div class="faq-body">
-					Whenever necessary, you can update your details by clicking your [Account] area located
-					at the top right hand side of the page
-				</div>
-			</div>
-
-			<div class="faq-info" id="q2d">
-				<div class="faq-header">
-					F. HOW TO LOGIN?
-				</div>
-				<div class="faq-body">
-					<ul style="list-style:none;">
-						<li>Click [LOGIN] located in the top right corner of every page.</li>
-						<li>A login panel will open up.</li>
-						<li>Enter the email address you signed up with and your password</li>
-						<li>Finally, click the LOGIN button.</li>
-					</ul>
-				</div>
-			</div>
-			<div class="faq-info" id="q2e">
-				<div class="faq-header">
-					G. DELETING YOUR ACCOUNT
-				</div>
-				<div class="faq-body">
-					We’d be really sad to see you go! But if you must, please send an email to <a href="mailto:info@pretastyler.com">info@pretastyler.com</a> requesting your account be deleted and we will personally make sure your account is removed promptly.  Be sure to also give us the email address you use on the site and tell us whether or not you still want to receive our company updates. In addition you will need to stop your monthly payments through Paypal
-				</div>
-			</div>
-
-			<div class="faq-info" id="q3">
-				<div class="faq-header">
-					YOUR PASSWORD
-				</div>
-				<div class="faq-body">
-					&nbsp;&nbsp;&nbsp;a. Forgetten Your Password?<br>
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No problem, just follow these easy steps:
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<p>
-					<ol>
-						<li>Click the ‘Forgot Your Password’ link below the LOG IN button or <a href="/useraccount/login.html">HERE</a>.</li>
-						<li>On the next screen, you will be asked to enter your email address.</li>
-						<li>Click Send.</li>
-						<li>You will receive and email to confirm the request is from you with a reset link.  If you don’t at first see it remember to check your junk/spam folder.</li>
-					</ol>
-				</p>
-			</div>
-			<div class="faq-info" id="q4">
-				<div class="faq-header">
-					A. HOW DO I SIGN UP AND WHAT'S REQUIRED?
-				</div>
-				<div class="faq-body">
-					<p>Registration is through this [page] and costs $9 per month.</p>
-					<p>	Once you have registered you will be lead through the profile and preferences set-up. Upon completion your personalized mall will be stocked and the fun begins!
-						<p>1. Click <a href="/index.html#profile">HERE</a> or the SIGN-UP link on the top right of every page. (You can also sign up using any of the signup buttons throughout the website)
-						</p><p>	2. Enter you Name and Email
-					</p><p>	3. Chosse a Password (must be alphanumberic - letters and numbers only)
-				</p><p>4. Confirm the same Password</p>
-			</div>
-			<div class="faq-info" id="q5">
-				<div class="faq-header">
-					A. HOW DO I SIGN UP AND WHAT'S REQUIRED?
-				</div>
-				<div class="faq-body">
-					<p>Registration is through this [page] and costs $9 per month.</p>
-					<p>	Once you have registered you will be lead through the profile and preferences set-up. Upon completion your personalized mall will be stocked and the fun begins!
-						<p>1. Click <a href="/index.html#profile">HERE</a> or the SIGN-UP link on the top right of every page. (You can also sign up using any of the signup buttons throughout the website)
-						</p><p>	2. Enter you Name and Email
-					</p><p>	3. Chosse a Password (must be alphanumberic - letters and numbers only)
-				</p><p>4. Confirm the same Password</p>
-			</div>
-			<div class="faq-info" id="q6">
-				<div class="faq-header">
-					A. HOW DO I SIGN UP AND WHAT'S REQUIRED?
-				</div>
-				<div class="faq-body">
-					<p>Registration is through this [page] and costs $9 per month.</p>
-					<p>	Once you have registered you will be lead through the profile and preferences set-up. Upon completion your personalized mall will be stocked and the fun begins!
-						<p>1. Click <a href="/index.html#profile">HERE</a> or the SIGN-UP link on the top right of every page. (You can also sign up using any of the signup buttons throughout the website)
-						</p><p>	2. Enter you Name and Email
-					</p><p>	3. Chosse a Password (must be alphanumberic - letters and numbers only)
-				</p><p>4. Confirm the same Password</p>
 			</div>
 		</div>
 	</div>
-	<div class="half">
-		<div class="faqs">
-			<div class="faqs-header">FAQs:</div>
-			<div class="tab-links">
-				<div class="active"><a href="#q1"><strong>WHAT IS PRÊTÀSTYLER?</strong></a></div>
-
-				<div style="font-style:italic;">
-					<a href="#q2"><strong>SIGNING UP/ MEMBERSHIP</strong></a>
-				</div>
-				<div style="padding:10px 50px;">
-					<ul>
-						<li><a href="#q2" class="i">a. How Do I Sign Up and What's Required?</a></li>
-						<li><a href="#q2a" class="i">b. What's in the sneaky fine print, where's the catch?</a></li>
-						<li><a href="#q2b" class="i">c. Is There a Free Version?</a></li>
-						<li><a href="#q2b" class="i">d. Free Trials</a></li>
-						<li><a href="#q2c" class="i">e. Updating Your Details</a></li>
-						<li><a href="#q2d" class="i">f. How to Login</a></li>
-						<li><a href="#q2e" class="i">g. Deleting Your Account</a></li>
-					</ul>
-				</div>
-				<div><a href="#q3"><strong>YOUR PASSWORD</strong></a></div>
-				<div><a href="#q4"><strong>YOUR PROFILE</strong></a></div>
-				<div><a href="#q5"><strong>THE MALL</strong></a></div>
-				<div><a href="#q6"><strong>OUR SEARCHES</strong></a></div>
-				<div><a href="#q7"><strong>STYLE GENIE APP</strong></a></div>
-				<div><a href="#q8"><strong>STYLE CLINIC</strong></a></div>
-				<div><a href="#q9"><strong>BUGS, SUGGESTIONS & FEEDBACK</strong></a></div>
-				<div><a href="#q10"><strong>MY PRIVATE STYLIST</strong></a></div>
-			</div>
-		</div>
-	</div>
-</article>
-<div class="clear"></div>
+</div>
