26c961d9cf9e3dbee33668cfe884360652ad27e5
Anil Prz <anil@asmex.com.au>
2015-04-07 13:30:17 +1000

    added image extension Signed-off-by: Anil Prz <anil@asmex.com.au>

--- application/views/mall/garments.php
+++ application/views/mall/garments.php
@@ -34,8 +34,8 @@ foreach ($garments as $row) { ?>
 				<?php } ?>
 			</ul>
 			<a href="/product/<?php print $row['garment_id'].'-'.url_title($row['name']).'.html' ?>" class="zoom bottomleftimage-showPink" target="_blank" title="Large View/Product details">
-				<img src="/images/icons-25-info-pink" class="imageiconinfo-pink bottomleftimage-mall" style="display:none;">
-				<img src="/images/icons-25-info-grey" class="imageiconinfo-grey bottomleftimage-mall">
+				<img src="/images/icons-25-info-pink.png" class="imageiconinfo-pink bottomleftimage-mall" style="display:none;">
+				<img src="/images/icons-25-info-grey.png" class="imageiconinfo-grey bottomleftimage-mall">
 			</a>
 			<?php if ($this->flexi_auth->is_logged_in() && isset( $row['score'] ) ) {?>
 			<div class="rating" title="Suitability rating for you"><?php 
