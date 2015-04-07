cc919d2b68746836404e65d70f7d0d362ac24a88
diwaskandel <diwas@asmex.com.au>
2015-04-07 14:55:24 +1000

    description added to aoc

--- application/views/catalog/yourmall.php
+++ application/views/catalog/yourmall.php
@@ -9,7 +9,7 @@
 				
 				<script type="text/javascript">
 					
-					var default_values = [ 4,2,8,3,2,1,2,1,2,1,3,1];
+					var default_values = [ 4,2,8,3,2,1,2,1,2,1,2,1];
 					
 					$(function(){
 						
