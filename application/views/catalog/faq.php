d06fb4adb3082fba49289737cfe1c3297c2e7bf0
diwaskandel <diwas@asmex.com.au>
2015-04-07 12:07:08 +1000

    help page changes completed

--- application/views/catalog/faq.php
+++ application/views/catalog/faq.php
@@ -43,10 +43,10 @@
 					<div class="faq-body">
 						Registration is through this [page] and costs $9 per month.<br>
 						Once you have registered you will be lead through the profile and preferences set-up. Upon completion your personalized mall will be stocked and the fun begins!<br>
-						1. Click <a href="/index.html#profile">HERE</a> or the SIGN-UP link on the top right of every page. (You can also sign up using any of the signup buttons throughout the website)<br>
-						2. Enter you Name and Email<br>
-						3. Chosse a Password (must be alphanumberic - letters and numbers only)<br>
-						4. Confirm the same Password
+						<ol><li> Click <a href="/index.html#profile">HERE</a> or the SIGN-UP link on the top right of every page. (You can also sign up using any of the signup buttons throughout the website)<br>
+						</li><li> Enter you Name and Email</li>
+						<li>Chosse a Password (must be alphanumberic - letters and numbers only)</li>
+						<li> Confirm the same Password</li></ol>
 					</div>
 				</div>
 				
