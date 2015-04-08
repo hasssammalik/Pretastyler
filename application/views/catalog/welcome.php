5c9b31b2727082bb062466942b1f92e80ca118a7
diwaskandel <diwas@asmex.com.au>
2015-04-08 13:48:51 +1000

    welcome page

--- application/views/catalog/welcome.php
+++ application/views/catalog/welcome.php
@@ -1,4 +1,4 @@
-<div class="mainContent ">
+<!--<div class="mainContent ">
 <div class="welcomepage"></div>
 <div class="welcome-content">
 <div class="welcome-head">welcome user</div>
@@ -31,4 +31,34 @@ $(document).ready(function(){
 	$('.welcomepage').css("height", $(document).height());
 	$('.welcomepage').css("width", $(document).width());
 })
-</script>
+</script> -->
+<div class="mainContent">
+
+	<div class="infoBox successpage center">
+		<div>
+			<h3>THANK YOU !</h3>
+			<p>
+				You have now joined the style revolution.<br>
+				Your personalised mall is almost ready for you.<br>
+				Click below to complete your body profile<br>
+			
+			</p>
+			
+			<p class="center">
+				<a class="bkpinkycolor" href="/user.html"> COMPLETE BODY PROFILE </a>
+			</p>
+			
+			<p>
+				Completed the body profile already? Lucky You!<br>
+				You can go straight to your mall and see what fabulous<br>
+				garments the style genie has picked out for you<br>
+			</p>
+			
+			<p class="center">
+				<a class="bkgrey" href="/mall.html">VISIT MALL</a>
+			</p>
+			
+		</div>
+	</div>
+
+</div>
