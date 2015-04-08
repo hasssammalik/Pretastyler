6770ca032968eb1611c18a90b133de9ad3d13c96
diwaskandel <diwas@asmex.com.au>
2015-04-08 12:47:02 +1000

    editing welcome page

--- application/views/catalog/welcome.php
+++ application/views/catalog/welcome.php
@@ -1,4 +1,4 @@
 <div class="mainContent">
  <?php if ($this->flexi_auth->is_logged_in()){ ?>
-<strong><?php echo "welcome"; ?> </strong>
+<strong><?php echo $first_name; ?> </strong>
  <?php } ?></div>
