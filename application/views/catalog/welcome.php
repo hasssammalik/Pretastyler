ba46db6a53ab26a6a8d180e26deacfa3b409f494
diwaskandel <diwas@asmex.com.au>
2015-04-08 13:32:42 +1000

    welcome page

--- application/views/catalog/welcome.php
+++ application/views/catalog/welcome.php
@@ -1,4 +1,5 @@
-<div class="mainContent welcomepage">
+<div class="mainContent ">
+<div class="welcomepage"><img src="/images/welcome-bg.jpg" alt=""/></div>
 <div class="welcome-content">
 <div class="welcome-head">welcome user</div>
 <div class="welcome-body">We're thrilled to have you as part of our family of savvy fashion shoppers. <br> We've placed lots of cool features in your mall so don't forget to hover your mouse over tabs and icons to see what they do. </div>
@@ -10,6 +11,7 @@
 	background-size: cover;
 }
 .welcome-content{
+	position:fixed;
 	width:400px;
 	margin:200px auto;
 }
