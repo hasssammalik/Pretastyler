1841d9e29e9b9255f942d5fa704e7a75ecc2054e
Anil Prz <anil@asmex.com.au>
2015-04-28 17:35:44 +1000

    Signed-off-by: Anil Prz <anil@asmex.com.au>

--- application/views/admin/garment/general.php
+++ application/views/admin/garment/general.php
@@ -82,9 +82,16 @@ $(function(){
 	
 	$(document).on({
 		mouseenter: function(){
+			
 			var real_img = $(this).attr("src");
 			$(this).parent("td").css("position", 'relative');
-			$(this).parent("td").append('<div class="hoverBigImageImage" style="position: absolute; top: -150px; left: 48px; max-width: 500px; min-width: 400px; padding: 6px; z-index: 2;"><img src="'+real_img+'" style="width: 100% !important;"></div>').fadeIn("fast");
+			$(this).parent("td").append('<div class="hoverBigImageImage" style="position: absolute; top: -150px; left: 48px; max-width: 400px; min-width: 300px; padding: 6px; z-index: 2;"><img src="'+real_img+'" style="width: 100% !important;"></div>').fadeIn("fast");
+			if ($(this).parent("td").position().top+ $(this).next().find('img').height() + 150>= $(window).height()){
+				$(this).next().css('top', ($(window).height() - $(this).next().find('img').height() - $(this).parent("td").position().top - 150)+'px');
+			} else {
+				$(this).next().css('top', '0px');
+			}
+			
 		},
 		mouseleave: function(){
 			$( ".hoverBigImageImage" ).fadeOut("fast").remove();
