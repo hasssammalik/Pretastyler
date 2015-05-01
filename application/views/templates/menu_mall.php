13b3f14ff80a3d3d5664021a85c3126950f6fbd2
Jifeng Sun <robertjajajja@gmail.com>
2015-05-01 19:06:45 +1000

    Signed Off by Jifeng Sun

--- application/views/templates/menu_mall.php
+++ application/views/templates/menu_mall.php
@@ -14,9 +14,39 @@
 	</script>
 
 </section>
-<div class="headPageTitle">
-	<?php echo strtoupper($title) ?>
+if (!$this->flexi_auth->is_logged_in()){
+	$newClass="nomadpage";
+}
+if( !empty( $breadcrumb[0] ) ) { ?>
+
+<div class="<?php if(isset($newClass)){echo $newClass;} ?>">
+	<?php  if(isset($newClass)){ ?>
+	<div class="nomad-banner">
+		<div class="nomad-banner-image">
+			<img src="/images/mall-warning.png">
+		</div>
+		<div class="nomad-banner-text bkpinkycolor">
+			WARNING: This mall is not OPTIMIZED!<br>Test drive a customised mall today<br>
+			<a href="/#profile"><i>	Discover the Possibilities </i></a>
+		</div>
+		<div class="nomad-banner-image">
+			<img src="/images/mall-warning.png">
+		</div>
+		<div class="clear">
+		</div>
+	</div>
+	<?php } ?>
 </div>
+<br>
+<?php } ?>
+<?php
+echo form_open(); echo form_close();
 
-
-
+ if( !empty( $breadcrumb[0] ) ) { ?>
+	<div>
+		<div class="headPageTitle">
+		   <?php echo strtoupper($title) ?>
+		</div>
+	</div>
+<br>
+<?php } ?>
