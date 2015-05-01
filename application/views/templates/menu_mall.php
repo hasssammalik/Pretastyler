5f6c41d9adf0eba6feb4adca16ac98572e1e805e
diwaskandel <diwas@asmex.com.au>
2015-05-01 12:44:46 +1000

    working on nomad page banner

--- application/views/templates/menu_mall.php
+++ application/views/templates/menu_mall.php
@@ -21,7 +21,7 @@ if (!$this->flexi_auth->is_logged_in()){
 }
  if( !empty( $breadcrumb[0] ) ) { ?>
 
-	<div class="<?php echo $newClass; ?>">
+	<div class="<?php if(isset($newClass)){echo $newClass;} ?>">
 		<div class="headPageTitle ">
 		   <?php echo strtoupper($title) ?>
 		</div>
