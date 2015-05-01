a4dd9ff428b96ec36d25e2063fdb8145931a0646
Jifeng Sun <robertjajajja@gmail.com>
2015-05-01 16:57:21 +1000

    Signed Off by Jifeng Sun

--- application/models/category_model.php
+++ application/models/category_model.php
@@ -63,7 +63,22 @@ class Category_model extends CI_Model{
 		if ( $query->num_rows() < 1 ) return array();
 		return $query->result_array();
 	}
-	
+	/**
+	 * get_length
+	 * Read category's
+	 * Returning category info
+	 *
+	 * @return an array of all the categories present in the database.
+	 */
+	public function get_length($category_id) {
+		$query = $this->db->select('category.category_id AS id, category.`name`, category_field.field_id')->from('category')->join('category_field', 'category.category_id = category_field.category_id', 'left')->join('field', 'category_field = field.field_id', 'left')->where('category.category_id', $category_id)->order_by('field.position DESC')->limit(1)->get();
+		$length = $query->row_array();
+		
+		$query = $this->db->select('criteria_id AS id, `name`')->from('criteria')->where('field_id', $length['field_id'])->order_by('position')->get();
+		$length['criterias'] = $query->result_array();
+		unset($length['field_id'])
+		return $length;
+	}	
 }
 
 /* End of file category_model.php */
