caacbe27905968d9cc2726cf8d61aa1a157fdd45
Anil Prz <anil@asmex.com.au>
2015-05-01 18:30:25 +1000

    Signed-off-by: Anil Prz <anil@asmex.com.au>

--- application/views/mall/quicksearch_length.php
+++ application/views/mall/quicksearch_length.php
@@ -1,25 +1,23 @@
 <?php 
 if( !empty( $category_lengths ) ) {
-	foreach ( $category_lengths as $category ) {
 ?>
-		<div id="quick-search-category-length-section-<?php echo $category['id'] ?>">
-			<hr class="widfulled">	
-			<div class="col widfulled">
-				<label><?php echo $category['name'] ?>:</label>
-				<fieldset class="checkboxes otherSection quick-<?php echo $category['name'] ?>-length" 
-							data-cat-length-name="<?php echo $category['name'] ?>" 
-							data-cat-length-id="<?php echo $category['id'] ?>" 
-							id="checkbox-search-<?php echo $category['name'] ?>-<?php echo $category['id'] ?>-length">
-					<?php foreach ($category['criterias'] as $row) { ?>
-						<label>
-							<input type="checkbox" value="<?php print $row['id'] ?>">
-							<span title="<?php print $row['name'] ?>"><?php print $row['name'] ?></span>
-						</label>
-					<?php } ?>
-				</fieldset>
-			</div>
+	<div id="quick-search-category-length-section-<?php echo $category_lengths['id'] ?>">
+		<hr class="widfulled">	
+		<div class="col widfulled">
+			<label><?php echo $category_lengths['name'] ?>:</label>
+			<fieldset class="checkboxes otherSection quick-<?php echo $category_lengths['name'] ?>-length" 
+						data-cat-length-name="<?php echo $category_lengths['name'] ?>" 
+						data-cat-length-id="<?php echo $category_lengths['id'] ?>" 
+						id="checkbox-search-<?php echo $category_lengths['name'] ?>-<?php echo $category_lengths['id'] ?>-length">
+				<?php foreach ($category_lengths['criterias'] as $row) { ?>
+					<label>
+						<input type="checkbox" value="<?php print $row['id'] ?>">
+						<span title="<?php print $row['name'] ?>"><?php print $row['name'] ?></span>
+					</label>
+				<?php } ?>
+			</fieldset>
 		</div>
+	</div>
 <?php
-	}
 }
 ?>
