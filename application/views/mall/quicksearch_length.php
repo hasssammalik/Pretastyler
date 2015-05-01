ce933ba180558be04f6a3fea133661436d749bde
Anil Prz <anil@asmex.com.au>
2015-05-01 18:17:53 +1000

    Signed-off-by: Anil Prz <anil@asmex.com.au>

--- application/views/mall/quicksearch_length.php
+++ application/views/mall/quicksearch_length.php
@@ -1,16 +1,16 @@
 <?php 
-if( !empty( $category_with_lengths ) ) {
-	foreach ( $category_with_lengths as $categories ) {
+if( !empty( $category_lengths ) ) {
+	foreach ( $category_lengths as $category ) {
 ?>
 		<div id="quick-search-category-length-section-<?php echo $categories['id'] ?>">
 			<hr class="widfulled">	
 			<div class="col widfulled">
-				<label><?php echo $categories['name'] ?>:</label>
-				<fieldset class="checkboxes otherSection quick-<?php echo $categories['name'] ?>-length" 
-							data-cat-length-name="<?php echo $categories['name'] ?>" 
-							data-cat-length-id="<?php echo $categories['id'] ?>" 
-							id="checkbox-search-<?php echo $categories['name'] ?>-<?php echo $categories['id'] ?>-length">
-					<?php foreach ($categories['criterias'] as $row) { ?>
+				<label><?php echo $category['name'] ?>:</label>
+				<fieldset class="checkboxes otherSection quick-<?php echo $category['name'] ?>-length" 
+							data-cat-length-name="<?php echo $category['name'] ?>" 
+							data-cat-length-id="<?php echo $category['id'] ?>" 
+							id="checkbox-search-<?php echo $category['name'] ?>-<?php echo $category['id'] ?>-length">
+					<?php foreach ($category['criterias'] as $row) { ?>
 						<label>
 							<input type="checkbox" value="<?php print $row['id'] ?>">
 							<span title="<?php print $row['name'] ?>"><?php print $row['name'] ?></span>
