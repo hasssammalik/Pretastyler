0f3d848f398aa374cecca7491d8fb11e87ef717e
Anil Prz <anil@asmex.com.au>
2015-05-04 14:47:49 +1000

    Signed-off-by: Anil Prz <anil@asmex.com.au>

--- application/views/catalog/useraccount.php
+++ application/views/catalog/useraccount.php
@@ -15,7 +15,7 @@
 						<div class=" loginPanel">
 							<div class="group">
 								<div class="col" style="position:relative;">
-								<?php if ( uri_string() == '' ) { $link_url_to = "index"; } else {  $link_url_to = uri_string(); } ?>
+								<?php if ( strpos( $_SERVER['HTTP_REFERER'], 'pretastyler.com' ) !== false ) { $link_url_to = $_SERVER['HTTP_REFERER']; } else {  $link_url_to = '/mall.html'; } ?>
 									<div id="login-form" return_url="/<?php print $link_url_to ?>.html">
 										<?php  $attlog = array('name' => 'menu_mall_login', 'id' => 'menu_mall_login');echo form_open('/useraccount/login.html', $attlog); ?>
 										
