3eaa31a239dfec0d29ee64f8b907278eb4cfed54
Anil Prz <anil@asmex.com.au>
2015-05-04 14:59:24 +1000

    Login Return Url fix. Signed-off-by: Anil Prz <anil@asmex.com.au>

--- application/views/catalog/useraccount.php
+++ application/views/catalog/useraccount.php
@@ -16,13 +16,19 @@
 							<div class="group">
 								<div class="col" style="position:relative;">
 								<?php 
-									if ( strpos( $_SERVER['HTTP_REFERER'], 'pretastyler.com' ) !== false ) 
-									{ 
-										$link_url_to = $_SERVER['HTTP_REFERER'];
-										$link_url_to = explode( 'pretastyler.com', $link_url_to );
-										$link_url_to = $link_url_to[1];
+									if( !empty( $_SERVER['HTTP_REFERER'] )){
+										if ( strpos( $_SERVER['HTTP_REFERER'], 'pretastyler.com' ) !== false ) 
+										{ 
+											$link_url_to = $_SERVER['HTTP_REFERER'];
+											$link_url_to = explode( 'pretastyler.com', $link_url_to );
+											$link_url_to = $link_url_to[1];
 
-									} else {  $link_url_to = '/mall.html'; } ?>
+										} else {  $link_url_to = '/mall.html'; }
+									} else {
+										$link_url_to = '/mall.html';
+									}
+
+								?>
 									<div id="login-form" return_url="<?php print $link_url_to ?>">
 										<?php  $attlog = array('name' => 'menu_mall_login', 'id' => 'menu_mall_login');echo form_open('/useraccount/login.html', $attlog); ?>
 										
