be4730b4370b15101ab41dcc1b44212f9362233b
Jifeng Sun <robertjajajja@gmail.com>
2015-05-13 16:37:56 +1000

    Peter's SEO work
    Signed Off by Jifeng Sun

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -81,13 +81,14 @@ f=false,d=document;return{use_existing_jquery:function(){return use_existing_jqu
 
 <?php if( ENVIRONMENT == 'production') { ?>
 	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-51485487-1', 'auto');
-	  ga('send', 'pageview');
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//http://www.google-analytics.com/analytics.js','ga')
+		ga('create', 'UA-51485487-1', ‘auto’, {'allowLinker': true});
+		ga('require', 'linker');
+		ga('linker:autoLink', [‘pretastyler.com/blog'] );
+		ga('send', 'pageview');
 	</script>
 	<script type="text/javascript" src="//s.skimresources.com/js/66930X1514478.skimlinks.js"></script>
 <?php } ?>
