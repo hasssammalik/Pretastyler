4867a53f150d7da7368f15d5f4a0a7b910164dd6
Anil Prz <anil@asmex.com.au>
2015-05-14 13:23:24 +1000

    Signed-off-by: Anil Prz <anil@asmex.com.au>

--- application/controllers/garment.php
+++ application/controllers/garment.php
@@ -653,8 +653,26 @@ class Garment extends CI_Controller {
 		$images = $this->session->userdata( 'upload_data_images');
 		if( !empty( $images )) {
 			if ( $imagedata == 'get' ){
-				$this->load->view('garment/import_garment_images', array('images' => $images) );
+				//$this->load->view('garment/import_garment_images', array('images' => $images) );
 				$this->session->unset_userdata('upload_data_images'); 
+
+				foreach ($images as $image_key => $image) {
+					if (strpos( $image->src,'static.theiconic.com.au%2Fp%2F') !== false) {
+						$imageNew = explode( 'static.theiconic.com.au%2Fp%2F', $image->src );
+						$images[$image_key]->src = 'http://static.theiconic.com.au/p/'.$imageNew[1];
+					    $imageProp = getimagesize( $images[$image_key]->src );
+					    $images[$image_key]->width = $imageProp[0];
+					    $images[$image_key]->height   = $imageProp[1];
+					}
+					echo '<div class="item"><div class="itemName"><span><strong>SIZE:</strong>';
+					echo  $image->height.' x '. $image->width .'</span></div><div class="imgWrap"><div><a href="#"><img src="';
+					echo  $image->src '" alt="" class="image-choice"></a></div>';
+					if ($this->flexi_auth->in_group(array('Administrators', 'Uploaders'))) { 
+						echo '<div class="choiceText"></div>';
+					}
+					 echo '</div></div>';
+				}
+
 			} else {
 				
 				foreach ($images as $image_key => $image) {
