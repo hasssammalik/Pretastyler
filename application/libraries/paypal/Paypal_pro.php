86a8bb5f435085c8942528bbc1ff0168b87a0102
Jifeng Sun <robertjajajja@gmail.com>
2015-05-21 14:54:45 +1000

    convert to utf8

--- application/libraries/paypal/Paypal_pro.php
+++ application/libraries/paypal/Paypal_pro.php
@@ -3,7 +3,7 @@
  * 	Angell EYE PayPal NVP CodeIgniter Library
  *	An open source PHP library written to easily work with PayPal's API's
  *	
- *  Copyright � 2014  Andrew K. Angell
+ *  Copyright ?2014  Andrew K. Angell
  *	Email:  andrew@angelleye.com
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -21,7 +21,7 @@
  *
  * @package			Angell_EYE_PayPal_Class_Library
  * @author			Andrew K. Angell
- * @copyright		Copyright � 2014 Angell EYE, LLC
+ * @copyright		Copyright ?2014 Angell EYE, LLC
  * @link			http://www.angelleye.com
  * @since			Version 2.42
  * @updated			01.19.2014
@@ -92,7 +92,7 @@ class PayPal_Pro
 		
 		$this->Countries = array(
 							'Afghanistan' => 'AF',
-							'ÌÉland Islands' => 'AX',
+							'脤脡land Islands' => 'AX',
 							'Albania' => 'AL',
 							'Algeria' => 'DZ',
 							'American Samoa' => 'AS',
