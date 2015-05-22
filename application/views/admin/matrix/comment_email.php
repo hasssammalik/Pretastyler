6c384ca402cb4d6225130d7e955473d965beb368
Jifeng Sun <robertjajajja@gmail.com>
2015-05-22 14:16:51 +1000

    Signed Off by Jifeng Sun

--- application/views/admin/matrix/comment_email.php
+++ application/views/admin/matrix/comment_email.php
@@ -823,9 +823,10 @@ body.outlook p {
                       <table class="twelve columns">
                         <tr>
                           <td>
-                            <h1>Hi, Susan Calvin</h1>
-                						<p class="lead">Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae.</p>
-                						<p>Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae. consequat vel lacus. Sed iaculis pulvinar ligula, ornare fringilla ante viverra et. In hac habitasse platea dictumst. Donec vel orci mi, eu congue justo. Integer eget odio est, eget malesuada lorem. Aenean sed tellus dui, vitae viverra risus. Nullam massa sapien, pulvinar eleifend fringilla id, convallis eget nisi. Mauris a sagittis dui. Pellentesque non lacinia mi. Fusce sit amet libero sit amet erat venenatis sollicitudin vitae vel eros. Cras nunc sapien, interdum sit amet porttitor ut, congue quis urna.</p>
+                            <h1>Hello, <?php print $user_info['first_name'].' '.$user_info['last_name']</h1>
+                						<p class="lead">Your following item has been corrected.</p>
+                						<p>Stylefully yours,</p>
+                						<p>Ann Reinten</p>
                           </td>
                           <td class="expander"></td>
                         </tr>
