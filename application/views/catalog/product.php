73bf293af5907103788684dbe003ff735cda6d58
diwaskandel <diwas@asmex.com.au>
2015-05-22 10:56:38 +1000

    fb share id added

--- application/views/catalog/product.php
+++ application/views/catalog/product.php
@@ -288,7 +288,7 @@ $star_result = array('Avoid', 'Avoid', 'Risky', 'Maybe', 'Good', 'Great' );
 										<ul class="bottomOptions">
 									
 									<li>
-										<a class="fb" id="fbs_share"><i class="icon-facebook"></i></a>
+										<a class="fb" id="fb_share"><i class="icon-facebook"></i></a>
 									</li>
 									
 									<li><a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());" target="_blank" class="pi"><i class="icon-pinterest"></i></a></li>
