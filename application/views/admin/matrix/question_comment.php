494cbba2ea3584f2b0ce67732cd301c90505a29a
Jifeng Sun <robertjajajja@gmail.com>
2015-05-22 11:31:30 +1000

    Signed Off by Jifeng Sun

--- application/views/admin/matrix/question_comment.php
+++ application/views/admin/matrix/question_comment.php
@@ -34,14 +34,18 @@
 						<?php foreach ($admin_comment['individuals'] as $key=>$value) {?>
 						<label>Comments - <?php print $value['field_name'] ?> - Click <a href="/admin/matrix/field/edit/<?php print $value['field_id']?>.html" target="_blank">here</a> to this field.</label>
 						<div class="col-md-4">
-						<label>You Selected:</label>
+							<label>You Selected:</label>
+							<div class="form-control">
+							</div>
+						</div>
+							<div class="col-md-4">
+							<label>Changed To:</label>
+							<div class="form-control">
+							</div>
 						</div>
 						<div class="col-md-4">
-						<label>Changed To:</label>
-						</div>
-						<div class="col-md-4">
-						<label>Comments:</label>
-						<textarea class="form-control" rows="8" placeholder="Enter Comments for <?php print $value['field_name'] ?>" name="overall-comments"><?php print $value['content'] ?></textarea>
+							<label>Comments:</label>
+							<textarea class="form-control" rows="8" placeholder="Enter Comments for <?php print $value['field_name'] ?>" name="overall-comments"><?php print $value['content'] ?></textarea>
 						</div>
 						<?php } ?>
 					</div>
