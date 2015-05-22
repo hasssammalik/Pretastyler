97a6f751cfbfb4ae78b313f18b0a628694034528
Jifeng Sun <robertjajajja@gmail.com>
2015-05-22 11:33:21 +1000

    Signed Off by Jifeng Sun

--- application/views/admin/matrix/question_comment.php
+++ application/views/admin/matrix/question_comment.php
@@ -32,20 +32,22 @@
 				<div class="box-body">
 					<div class="form-group">
 						<?php foreach ($admin_comment['individuals'] as $key=>$value) {?>
-						<label>Comments - <?php print $value['field_name'] ?> - Click <a href="/admin/matrix/field/edit/<?php print $value['field_id']?>.html" target="_blank">here</a> to this field.</label>
-						<div class="col-md-4">
-							<label>You Selected:</label>
-							<div class="form-control">
-							</div>
-						</div>
+						<div class="form-control">
+							<label class="col-md-12">Comments - <?php print $value['field_name'] ?> - Click <a href="/admin/matrix/field/edit/<?php print $value['field_id']?>.html" target="_blank">here</a> to this field.</label>
 							<div class="col-md-4">
-							<label>Changed To:</label>
-							<div class="form-control">
+								<label>You Selected:</label>
+								<div class="form-control">
+								</div>
 							</div>
-						</div>
-						<div class="col-md-4">
-							<label>Comments:</label>
-							<textarea class="form-control" rows="8" placeholder="Enter Comments for <?php print $value['field_name'] ?>" name="overall-comments"><?php print $value['content'] ?></textarea>
+								<div class="col-md-4">
+								<label>Changed To:</label>
+								<div class="form-control">
+								</div>
+							</div>
+							<div class="col-md-4">
+								<label>Comments:</label>
+								<textarea class="form-control" rows="8" placeholder="Enter Comments for <?php print $value['field_name'] ?>" name="overall-comments"><?php print $value['content'] ?></textarea>
+							</div>
 						</div>
 						<?php } ?>
 					</div>
