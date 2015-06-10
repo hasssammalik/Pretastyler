7205d6c8bbb5fafa83ae7f974b909af6be0bc189
hassammalik <hassammalik@yahoo.com>
2015-06-10 13:52:02 +1000

    et

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -320,7 +320,7 @@ class Admin extends CI_Controller {
 			$this->datatables->where('pas_user_info.user_id', $user_id);
 		}
 
-		*/
+	
 		$this->datatables->edit_column('garments', '<a href="/admin/garment/user/$1.html">$2</a>', 'user_id, garments');
 		$this->datatables->edit_column('group_name', '$1 (<a href="/admin/user/change-group/$2.html">Change</a>)', 'group_name, user_id');
 		$this->datatables->edit_column('infusionsoft_id', '<a href="https://om185.infusionsoft.com/Contact/manageContact.jsp?view=edit&ID=$1" target="_blank">$1</a> (<a href="/admin/user/change-infusionsoft/$2.html">Change</a>)', 'infusionsoft_id, user_id');
