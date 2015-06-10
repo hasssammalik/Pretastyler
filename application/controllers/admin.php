6d7894ead25386089c1d86cb806f17ef09e4b959
hassammalik <hassammalik@yahoo.com>
2015-06-10 13:08:34 +1000

    test

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -293,7 +293,34 @@ class Admin extends CI_Controller {
 		$is_admin = $this->input->post('is_admin', TRUE);
 		$user_id = $this->input->post('user_id', TRUE);
 		//---- HM --- date_diff(NOW(),pas_user_accounts.uacc_date_last_login)
-		$this->datatables->select("pas_user_info.user_id, first_name, last_name, pas_user_accounts.uacc_email AS email, garment_info.garments, IF (uacc_active = 1, 'fa fa-check-circle', 'glyphicon glyphicon-ban-circle') AS active, pas_user_groups.ugrp_desc AS group_name, infusionsoft_id, pas_user_accounts.uacc_date_added AS creation_date, pas_user_accounts.uacc_date_last_login AS last_login, datediff(sysdate(), pas_user_accounts.uacc_date_last_login) as Days_Since_Last_Login", FALSE)->from('user_info')->join('user_accounts', 'user_info.user_id = user_accounts.uacc_id')->join('user_groups', 'user_groups.ugrp_id = user_accounts.uacc_group_fk')->join('user_infusionsoft', 'user_info.user_id = user_infusionsoft.user_id', 'left')->join('(SELECT import_user_id, COUNT(garment_id) AS garments FROM pas_garment GROUP BY import_user_id) AS garment_info', 'user_info.user_id = garment_info.import_user_id', 'left');
+	//old //	$this->datatables->select("pas_user_info.user_id, first_name, last_name, pas_user_accounts.uacc_email AS email, garment_info.garments, IF (uacc_active = 1, 'fa fa-check-circle', 'glyphicon glyphicon-ban-circle') AS active, pas_user_groups.ugrp_desc AS group_name, infusionsoft_id, pas_user_accounts.uacc_date_added AS creation_date, pas_user_accounts.uacc_date_last_login AS last_login, datediff(sysdate(), pas_user_accounts.uacc_date_last_login) as Days_Since_Last_Login", FALSE)->from('user_info')->join('user_accounts', 'user_info.user_id = user_accounts.uacc_id')->join('user_groups', 'user_groups.ugrp_id = user_accounts.uacc_group_fk')->join('user_infusionsoft', 'user_info.user_id = user_infusionsoft.user_id', 'left')->join('(SELECT import_user_id, COUNT(garment_id) AS garments FROM pas_garment GROUP BY import_user_id) AS garment_info', 'user_info.user_id = garment_info.import_user_id', 'left');
+		//-- HM --------------change select 
+	    $this->datatables->select("s1.user_id, first_name, last_name,  s1.email, s1.garments,  s1.active, s1.group_name, infusionsoft_id, s1.creation_date, s1.last_login, s1.Days_Since_Last_Login");
+	    $this->datatables->from("
+(
+select pas_user_info.user_id, first_name, last_name, pas_user_accounts.uacc_email AS email, garment_info.garments, IF (uacc_active = 1, 'fa fa-check-circle', 'glyphicon glyphicon-ban-circle') AS active, pas_user_groups.ugrp_desc AS group_name, infusionsoft_id, pas_user_accounts.uacc_date_added AS creation_date, pas_user_accounts.uacc_date_last_login AS last_login, datediff(sysdate(), pas_user_accounts.uacc_date_last_login) as Days_Since_Last_Login
+from pas_user_info join pas_user_accounts on pas_user_info.user_id = pas_user_accounts.uacc_id 
+join pas_user_groups on pas_user_groups.ugrp_id = pas_user_accounts.uacc_group_fk
+join pas_user_infusionsoft  on pas_user_info.user_id = pas_user_infusionsoft.user_id 
+left join 
+(
+SELECT import_user_id, COUNT(garment_id) AS garments FROM pas_garment GROUP BY import_user_id
+) garment_info 
+on pas_user_info.user_id = garment_info.import_user_id
+) s1 
+
+
+left join  
+
+
+(select email, count(*) login_per_Mnth
+from pas_user_logins
+where Month(Login) = Month (current_timestamp())
+group by email) s2 on s1.email = s2.email"
+);
+
+
+			/*
 		if ($is_standard) {
 			$this->datatables->where('pas_user_accounts.uacc_group_fk', 3);
 		}
@@ -309,6 +336,8 @@ class Admin extends CI_Controller {
 		if ($user_id) {
 			$this->datatables->where('pas_user_info.user_id', $user_id);
 		}
+
+		*/
 		$this->datatables->edit_column('garments', '<a href="/admin/garment/user/$1.html">$2</a>', 'user_id, garments');
 		$this->datatables->edit_column('group_name', '$1 (<a href="/admin/user/change-group/$2.html">Change</a>)', 'group_name, user_id');
 		$this->datatables->edit_column('infusionsoft_id', '<a href="https://om185.infusionsoft.com/Contact/manageContact.jsp?view=edit&ID=$1" target="_blank">$1</a> (<a href="/admin/user/change-infusionsoft/$2.html">Change</a>)', 'infusionsoft_id, user_id');
