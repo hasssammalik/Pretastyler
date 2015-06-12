f9de1efc6b786c424c0348281d887a9da61b6a6c
hassammalik <hassammalik@yahoo.com>
2015-06-12 12:48:05 +1000

    ht

--- application/views/garment/edit_Images.php
+++ application/views/garment/edit_Images.php
@@ -38,7 +38,90 @@
 					<div class="input-group">
 						<label for="garment-new-image">New Image</label>
 						<input type="file" name="new_image">
-						<p class="help-block">Upload a new image for this garnebt.</p>
+						<p class="help-block">Upload a first image.</p>
+					</div>
+				</div><!-- /.box-body -->
+			</div><!-- /.box -->
+		</div><!--/.col (left) -->
+
+
+		<!-- left column -->
+		
+		<div class="col-md-offset-4 col-md-4">
+		
+			<!-- general form elements -->
+			<div class="box box-primary">
+				<div class="box-header">
+					<h3 class="box-title">General Information</h3>
+				</div><!-- /.box-header -->
+				<!-- form start -->
+				<div class="box-body">
+					<div class="input-group">
+						<span class="input-group-addon">ID</span>
+						<input type="text" class="form-control" id="garment-id" value="<?php print $garment['garment_id'];?>" disabled>
+						<span class="input-group-addon">ID is not changeable</span>
+						<input type="hidden" name="garment_id" value="<?php print $garment['garment_id']; ?>">
+					</div>
+					<br/>
+					<div class="input-group">
+						<span class="input-group-addon">Name</span>
+						<input type="text" class="form-control" id="garment-name" placeholder="Enter garment Name" name="name" value="<?php print $garment['name'];?>">
+					</div>
+					<br/>
+										
+					<div class="input-group">
+						<label for="garment-image-path">Image</label>
+						<img src="/images/garment/<?php print $garment['image_path'] ?>">
+						<input type="hidden" name="has_new_image" value="0">
+						<input type="hidden" name="ori_image" value="<?php print $garment['image_path'] ?>">
+					</div>
+					<br/>
+					<div class="input-group">
+						<label for="garment-new-image">New Image</label>
+						<input type="file" name="new_image">
+						<p class="help-block">Upload a first image.</p>
+					</div>
+				</div><!-- /.box-body -->
+			</div><!-- /.box -->
+		</div><!--/.col (left) -->
+
+
+
+		<!-- left column -->
+		
+		<div class="col-md-offset-4 col-md-4">
+		
+			<!-- general form elements -->
+			<div class="box box-primary">
+				<div class="box-header">
+					<h3 class="box-title">General Information</h3>
+				</div><!-- /.box-header -->
+				<!-- form start -->
+				<div class="box-body">
+					<div class="input-group">
+						<span class="input-group-addon">ID</span>
+						<input type="text" class="form-control" id="garment-id" value="<?php print $garment['garment_id'];?>" disabled>
+						<span class="input-group-addon">ID is not changeable</span>
+						<input type="hidden" name="garment_id" value="<?php print $garment['garment_id']; ?>">
+					</div>
+					<br/>
+					<div class="input-group">
+						<span class="input-group-addon">Name</span>
+						<input type="text" class="form-control" id="garment-name" placeholder="Enter garment Name" name="name" value="<?php print $garment['name'];?>">
+					</div>
+					<br/>
+										
+					<div class="input-group">
+						<label for="garment-image-path">Image</label>
+						<img src="/images/garment/<?php print $garment['image_path'] ?>">
+						<input type="hidden" name="has_new_image" value="0">
+						<input type="hidden" name="ori_image" value="<?php print $garment['image_path'] ?>">
+					</div>
+					<br/>
+					<div class="input-group">
+						<label for="garment-new-image">New Image</label>
+						<input type="file" name="new_image">
+						<p class="help-block">Upload a first image.</p>
 					</div>
 				</div><!-- /.box-body -->
 			</div><!-- /.box -->
