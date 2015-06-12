832c011f896fe192b81944514e7de80a5f214723
hassammalik <hassammalik@yahoo.com>
2015-06-12 11:03:45 +1000

    ewr

--- application/controllers/garment.php
+++ application/controllers/garment.php
@@ -713,7 +713,7 @@ class Garment extends CI_Controller {
 	}
 
 
-	public function ImageEdit(){
+	public function ImageEdit($slug = FALSE){
 		$this->load->library('user_check');
 		if (!$slug || !$this->flexi_auth->is_logged_in()) {
 			$this->not_found();
